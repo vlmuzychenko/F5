cbae5f400ad6ca9ae8ee28a35fe9a248c226dabd
vlmuzychenko <vlad.muzychenko@gmail.com>
2017-03-22 19:22:33 +0200

    event page changed

--- build/js/app.js
+++ build/js/app.js
@@ -10403,9 +10403,31 @@ $(function () {
 $(function () {
     $('.event-fp').fullpage({
         verticalCentered: true,
-        anchors: ['event', 'details', 'place', 'partners'],
+        anchors: ['event', 'details', 'place', 'partners', 'registration'],
         menu: '.yacht-wiki-menu',
-        css3: true
+        css3: true,
+        onLeave: function onLeave(anchorLink, index) {
+            if (index == 1) {
+                $('.yacht-wiki-menu').addClass('dark');
+                $('.yacht-wiki-menu').removeClass('light');
+            }
+            if (index == 2) {
+                $('.yacht-wiki-menu').addClass('dark');
+                $('.yacht-wiki-menu').removeClass('light');
+            }
+            if (index == 3) {
+                $('.yacht-wiki-menu').addClass('dark');
+                $('.yacht-wiki-menu').removeClass('light');
+            }
+            if (index == 4) {
+                $('.yacht-wiki-menu').addClass('dark');
+                $('.yacht-wiki-menu').removeClass('light');
+            }
+            if (index == 5) {
+                $('.yacht-wiki-menu').addClass('light');
+                $('.yacht-wiki-menu').removeClass('dark');
+            }
+        }
     });
 });
 
--- src/js/event.js
+++ src/js/event.js
@@ -1,8 +1,30 @@
 $(function() {
   $('.event-fp').fullpage({
     verticalCentered: true,
-		anchors: ['event', 'details', 'place', 'partners'],
+		anchors: ['event', 'details', 'place', 'partners', 'registration'],
 		menu: '.yacht-wiki-menu',
-    css3: true
+    css3: true,
+    onLeave: function(anchorLink, index){
+      if(index == 1) {
+        $('.yacht-wiki-menu').addClass('dark');
+        $('.yacht-wiki-menu').removeClass('light');
+      }
+      if(index == 2){
+        $('.yacht-wiki-menu').addClass('dark');
+        $('.yacht-wiki-menu').removeClass('light');
+      }
+      if(index == 3){
+        $('.yacht-wiki-menu').addClass('dark');
+        $('.yacht-wiki-menu').removeClass('light');
+      }
+      if(index == 4){
+        $('.yacht-wiki-menu').addClass('dark');
+        $('.yacht-wiki-menu').removeClass('light');
+      }
+      if(index == 5){
+        $('.yacht-wiki-menu').addClass('light');
+        $('.yacht-wiki-menu').removeClass('dark');
+      }
+    }
   });
 })

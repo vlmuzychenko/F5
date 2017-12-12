dc47d1ce0633cfdc5450dadf854332da00e8a19d
vlmuzychenko <vlad.muzychenko@gmail.com>
2017-12-12 18:10:31 +0200

    upd

--- build/js/price.js
+++ build/js/price.js
@@ -10,17 +10,33 @@ $(function () {
   var currentInfo = currentPrice.parent().data('price-info');
   var currentIncludedCont = currentInfo.included.split(',');
   var currentExtraCont = currentInfo.extra.split(',');
-  var currentImg = currentInfo.img;
+  var currentMulti = $.map(currentInfo.multiprice, function (value) {
+    return [value];
+  });
+  var item = $('.price-block__item');
 
   includedWrap.html('');
   extraWrap.html('');
   $('.price__title').text(currentText).addClass('active');
-  $('.skew-section__img').css('background-image', 'url(' + currentImg + ')');
+  item.each(function () {
+    $(this).addClass('price-animate');
+  });
   for (var i = 0; i < currentIncludedCont.length; i++) {
     includedWrap.append('<li class="active">' + currentIncludedCont[i] + '</li>');
   }
   for (var i = 0; i < currentExtraCont.length; i++) {
     extraWrap.append('<li class="active">' + currentExtraCont[i] + '</li>');
+  }
+  for (var i = 0; i < $('.price-block__item').length; i++) {
+    var text = currentMulti[i].text;
+    var price = currentMulti[i].price;
+    var current = currentMulti[i].current;
+
+    $(item[i]).find('.price-block__title').text(text);
+    $(item[i]).find('.price-block__value span').text(price);
+    if (current == 'y') {
+      $(item[i]).addClass('is-active');
+    }
   }
 
   $('.js-price label').click(function (e) {
@@ -31,17 +47,33 @@ $(function () {
     var info = target.parent().data('price-info');
     var includedCont = info.included.split(',');
     var extraCont = info.extra.split(',');
-    var img = info.img;
+    var multi = $.map(info.multiprice, function (value) {
+      return [value];
+    });
+    var item = $('.price-block__item');
 
     includedWrap.html('');
     extraWrap.html('');
     $('.price__title').text(text).removeClass('active');
-    $('.skew-section__img').css('background-image', 'url(' + img + ')');
+    item.each(function () {
+      $(this).removeClass('price-animate is-active');
+    });
     for (var i = 0; i < includedCont.length; i++) {
       includedWrap.append('<li>' + includedCont[i] + '</li>');
     }
     for (var i = 0; i < extraCont.length; i++) {
       extraWrap.append('<li>' + extraCont[i] + '</li>');
+    }
+    for (var i = 0; i < $('.price-block__item').length; i++) {
+      var text = multi[i].text;
+      var price = multi[i].price;
+      var current = multi[i].current;
+
+      $(item[i]).find('.price-block__title').text(text);
+      $(item[i]).find('.price-block__value span').text(price);
+      if (current == 'y') {
+        $(item[i]).addClass('is-active');
+      }
     }
     //animations
     setTimeout(function () {
@@ -58,6 +90,12 @@ $(function () {
       setTimeout(function () {
         self.addClass('active');
       }, i * 100);
+    });
+    item.each(function (i) {
+      var self = $(this);
+      setTimeout(function () {
+        self.addClass('price-animate');
+      }, i * 200);
     });
   });
 })();

b1fa478b6837f95d4200cf49ef67a1850f3b2c0c
vlmuzychenko <vlad.muzychenko@gmail.com>
2018-02-06 18:13:58 +0200

    upd

---
+++ src/js/onload.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+  var preloader = document.getElementById('preloader');
+  preloader.className += ' finish';
+  setTimeout(function(){
+    preloader.className += ' preloader-animate';
+  }, 100);
+  setTimeout(function(){
+    preloader.className += ' hide';
+  }, 500);
+};
